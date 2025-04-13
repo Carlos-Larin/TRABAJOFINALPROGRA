ec8f386264c4c4ae5d537b18d4fbf9f043522d60
Carlos-Larin <nekokar701@gmail.com>
2025-04-13 11:36:08 -0600

    ajustes

---
+++ trabajoFinalP/database/migrations/2025_04_13_170650_create_tabla_ventas.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tabla_ventas', function (Blueprint $table) {
+            $table->id();
+            $table->string('codigo_venta', 50)->unique();
+            $table->unsignedBigInteger('id_cliente');
+            $table->unsignedBigInteger('id_producto');
+            $table->integer('cantidad_producto');
+            $table->decimal('total_venta', 10, 2);
+            $table->date('fecha_venta');            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tabla_ventas');
+    }
+};
