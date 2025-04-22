29fabc3404c582811d40dc7375fccbf2910294be
Carlos-Larin <nekokar701@gmail.com>
2025-04-22 11:45:00 -0600

    la creacion del user

---
+++ trabajoFinalP/database/migrations/2025_04_22_173810_tbl_user.php
@@ -0,0 +1,32 @@
+<?php
+
+use GuzzleHttp\Promise\Create;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::Create('User', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre_usuario', 100);
+            $table->string('correo_electronico', 100)->unique();
+            $table->string('contraseña', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
