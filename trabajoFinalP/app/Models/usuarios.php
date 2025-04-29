3fd4d515eb543fc4f36466d971efa563de330a5b
Carlos-Larin <nekokar701@gmail.com>
2025-04-29 11:30:51 -0600

    Co-authored-by: DiegoZap <DiegoZap1@users.noreply.github.com>

---
+++ trabajoFinalP/app/Models/usuarios.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class usuarios extends Model
+{
+    //quince años tenia tu hermana
+
+    use HasFactory;
+
+    protected $table = 'User';
+
+    protected $fillable = [
+        'nombre_usuario',
+        'correo_electronico',
+        'contraseña',
+    ];
+
+    // Si necesitas relaciones, puedes definirlas aquí
+    public function producto()
+    {
+        return $this->belongsTo(Producto::class, '');
+    }
+}
